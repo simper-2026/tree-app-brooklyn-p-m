4821314578d5a34840df2bed7c36de2d0ae2ed5d
brooklyn-p-m <brooklyn.p.merrick@gmail.com>
2026-03-11 20:40:27 -0600

    made classes

--- Models/BinaryTree.cs
+++ Models/BinaryTree.cs
@@ -1,11 +1,38 @@
-﻿namespace Models;
+﻿using System.Dynamic;
+
+namespace Models;
 
 public class BinaryTree
 {
+    public void Insert(int value)
+    {
+        
+    }
+    public string InOrder()
+    {
+        return "";
+    }
+    public int Height()
+    {
+        return 0;
+    }
+    public string ToMermaid()
+    {
+        return "";
+    }
 
 }
 
 public class Node
 {
-    
+    public int Value { get; set; }
+    public Node? Left;
+    public Node? Right;
+
+    public Node(int value, Node right, Node left)
+    {
+        Value = value;
+        Right = right;
+        Left = left;
+    }
 }
