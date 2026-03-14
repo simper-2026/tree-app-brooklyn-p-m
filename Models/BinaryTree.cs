0c6ed2e5a0fa4aa33584d03f9e61a51edb1db10c
brooklyn-p-m <brooklyn.p.merrick@gmail.com>
2026-03-13 18:10:36 -0600

    finished mermaid

--- Models/BinaryTree.cs
+++ Models/BinaryTree.cs
@@ -1,5 +1,6 @@
 ﻿using System.Dynamic;
 using System.IO.Pipelines;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace Models;
@@ -84,26 +85,12 @@ public class BinaryTree
     {
         if (current != null)
         {
-            int left = 0;
-            int right = 0;
-            if (current.Left != null)
-            {
-                left = Height(current.Left, left);
-            }
-            if (current.Right != null)
-            {
-                right = Height(current.Right, right);
-            }
-            if (left > right)
-            {
-                value += right;
-            }
-            else
-            {
-                value += left;
-            }
+            int left = 0, right = 0;
+            left = current.Left is null ? 0 : Height(current.Left, left);
+            right = current.Right is null ? 0 : Height(current.Right, right);
+            return left > right ? left + 1 : right + 1;
         }
-        return value;
+        return 0;
     }
     public string ToMermaid()
     {
@@ -115,7 +102,9 @@ public class BinaryTree
         {
             return $"graph TD\n{root.Value}";
         }
+
         int links = 0;
+
         return $"graph TD\n{ToMermaid(root!, ref links)}";
     }
     private string ToMermaid(Node current, ref int links)
@@ -125,23 +114,33 @@ public class BinaryTree
             return string.Empty;
         }
         string result = string.Empty;
+
+
         if (current.Left != null)
         {
             result += $"{current.Value} --> {current.Left.Value}\n";
+            links++;
             result += ToMermaid(current.Left, ref links);
         }
         else
         {
-            result += $"{current.Value} --> FIX\n";
+            result += $"{current.Value} --> _phl{current.Value}[ ]\n";
+            result += $"linkStyle {links} stroke:none, stroke-width:0, fill:none\n";
+            result += $"style _phl{current.Value} fill:none,stroke:none,color:none\n";
+            links++;
         }
         if (current.Right != null)
         {
             result += $"{current.Value} --> {current.Right.Value}\n";
+            links++;
             result += ToMermaid(current.Right, ref links);
         }
         else
         {
-            result += $"{current.Value} --> FIX\n";
+            result += $"{current.Value} --> _phr{current.Value}[ ]\n";
+            result += $"linkStyle {links} stroke:none, stroke-width:0, fill:none\n";
+            result += $"style _phr{current.Value} fill:none,stroke:none,color:none\n";
+            links++;
         }
 
         return result;
